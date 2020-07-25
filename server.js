e58237a1db8895675ee0d8e558dbd8298e16eeb3
L10 <49228795+L10Messi10@users.noreply.github.com>
2020-07-25 12:35:00 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = require('express')(); // Create an instance of an Express app
 var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobile App with default settings
 
 mobileApp.tables.add('TBL_Users'); // Create a table for 'Book' with default settings
-mobileApp.tables.add('UserBalance'); // Create a table for 'Book' with default settings
+mobileApp.tables.add('TBL_Category'); // Create a table for 'Book' with default settings
 mobileApp.tables.add('ViewUserBalance');
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
